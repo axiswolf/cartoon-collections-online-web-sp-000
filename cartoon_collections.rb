cf9844f1dd802430fdb835f92dec70e72de97b52
Karen V <karenvo@usc.edu>
2019-02-25 09:55:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,12 @@ def long_planeteer_calls(words)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if ingredients.include?(cheese_types)
+    true
+  else
+    false
+  end
 end
